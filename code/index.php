0c57658c6caeaad914f8442d6b2c6ce04faefaa7
Tianxu Xi <tianxu.xi@student.adelaide.edu.au>
2017-03-10 23:09:43 +1030

    version_2_uncompleted

--- code/index.php
+++ code/index.php
@@ -1,32 +1,53 @@
 <?php
 $var=array("x", "y", "z");
-$counter=0;
+$count_line=0;         //how many line so far
+$count_text=0;         //the number of current text cell
 ?>
 
 <form action="index.php" method="POST">
 <table id="table1" border="1">
-　<tr>
+
+<!--the first line in the form -->
+<tr>
   <?php
+  echo "<td>statement(s)</td>";
   for($i=0;$i<sizeof($var);$i++)
   {
     echo "<td>$var[$i]</td>";
   }
    ?>
 </tr>
-<tr>
-  <?php
+
+<!--the second line in the form -->
+<?php
+  echo "<tr>";
+  echo "<td>s$count_line</td>";
+  $count_line++;
     for($i=0;$i<sizeof($var);$i++)
     {
-      echo '<td><input type="text" name=$text[$i]></td>';
+      echo '<td><input type="text" name=$text[$count_text]></td>';
+      $count_text++;
     }
+   echo "</tr>";
    ?>
-  　</tr>
-<?php
 
+<?php
 if(isset($_POST["add"]))
 {
-  clickadd();
+  for($i=0; $i<$count_line; $i++)
+  {
+  echo "<tr>";
+    echo "<td>s$count_line</td>";
+    $count_line++;
+      for($i=0;$i<sizeof($var);$i++)
+      {
+        echo '<td><input type="text" name=$text[$count_text]></td>';
+        $count_text++;
+      }
+  echo "</tr>";
+  }
 }
+/*
 function clickadd()
 {
   echo '<tr>';
@@ -35,13 +56,12 @@ function clickadd()
   echo '<td><input type="text" name="text"></td>';
   echo '</tr>';
 
-/*
   for($i=0;$i<sizeof($var);$i++)
   {
     echo '<td><input type="text" name="text"></td>';
   }
-*/
 }
+*/
 
  ?>
     </table><br><br>
