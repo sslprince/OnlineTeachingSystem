6bc7e067e7c279f3bef779f25b6baf5eaef17563
Changchang Liu <changchang.liu01@student.adelaide.edu.au>
2017-03-18 21:01:35 +1030

    upload shell and gdb file

---
+++ code/shell/test.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+cd 2017s1
+cd MP
+cd test1
+cd test1
+gcc -g main.c -o tet
+gdb tet < result.gdb
+
+name[0]=2
+index=false
+touch c.txt
+touch d.txt
+w=0
+while read myline
+do
+arr=(${myline// / })
+j=0
+for i in ${arr[@]}
+do
+j=$[j+1]
+#echo $j
+if echo $i | egrep -q '^[0-9]+$';
+then
+for z in ${name[@]}
+do
+echo $z
+if [ "$z" == "${arr[$[j-3]]}" ];
+then
+index=ture
+fi
+done
+fi
+done
+if [ "$index" == "false" ];
+then
+w=$[w+1]
+name[$w]=${arr[$[j-3]]}
+echo ${arr[$[j-3]]}>>d.txt
+else
+break
+fi
+done < b.txt
+
+
+
+while read myline
+do
+arr=(${myline// / })
+j=0
+for i in ${arr[@]}
+do
+j=$[j+1]
+#echo $j
+if echo $i | egrep -q '^[0-9]+$';
+then echo $i>>c.txt
+
+fi
+done
+done < b.txt
