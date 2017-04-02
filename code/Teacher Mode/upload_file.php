3fd1d70db7cfc0bfb26becb4785ee3e77c49e3e8
Changchang Liu <changchang.liu01@student.adelaide.edu.au>
2017-04-02 19:39:20 +0930

    change upload file

--- code/Teacher Mode/upload_file.php
+++ code/Teacher Mode/upload_file.php
@@ -1,10 +1,9 @@
 <?php include 'database.php';?>
+<?php include 'header.php';?>
 <?php session_start();?>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="utf-8" />
-<!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
@@ -15,7 +14,7 @@
 <title>Memory Trace Teacher Mode</title>
 
 <!-- Bootstrap core CSS -->
-<link href="css/bootstrap.css" rel="stylesheet">
+
 
 <!-- Custom styles for this template -->
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
@@ -27,53 +26,8 @@
 
 <body>
 
-<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-<button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<a class="navbar-brand" href="#">Teacher Mode</a>
 
-<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-<ul class="navbar-nav mr-auto">
-<li class="nav-item active">
-<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Student Mode</a>
-</li>
-
-<li class="nav-item">
-<a class="nav-link" href="#">Help</a>
-</li>
-</ul>
-<form class="form-inline mt-2 mt-md-0">
-<input class="form-control mr-sm-2" type="text" placeholder="Search">
-<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
-</div>
-</nav>
-
-<div class="container-fluid">
-<div class="row">
-<nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
-<ul class="nav nav-pills flex-column">
-<li class="nav-item">
-<a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="a.php">Upload C Code</a>
-
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Students' Marks</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Error Analytics</a>
-</li>
-</ul>
-
-</nav>
-
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 <div id = "container">
 
 <div id = "container1">
@@ -85,19 +39,19 @@
 <?php
     if ($_FILES["file"]["error"] > 0)
     {
-        echo "Error: " . $_FILES["file"]["error"] . "<br />";
+        echo "Error: please add a c file " . $_FILES["file"]["error"] . "<br />";
     }
     else
-    
+
     {
-        
+
         global $a;
         $c="";
         $b =$_FILES["file"]["name"];
         $myfile = fopen($_FILES["file"]["tmp_name"], "r") or die("Unable to open file!");
         while(!feof($myfile)) {
             $a = $a .fgets($myfile) . "<br>";
-            
+
         }
         echo $a;
         $query = "INSERT INTO file(name,source_file)VALUES('$b','$a')";
@@ -106,10 +60,10 @@
         $myfile1 = fopen($_FILES["file"]["tmp_name"], "r") or die("Unable to open file!");
         while(!feof($myfile1)) {
             $c = $c .fgets($myfile1);
-            
+
         }
         fclose($myfile1);
-        
+
         $myoutputfile = fopen("main.c", "w");
         fwrite($myoutputfile, $c);
         fclose($myoutputfile);
@@ -125,13 +79,13 @@
         }
         #＃$cmd = "sudo ./aa.sh";
         #$cmd1 = "sudo ./aa.sh";
-        
+
         #$cmd2 = "ls";
         #$output =exec($cmd);
         #system($cmd1,$output1);
         #echo $output;
-        
-        
+
+
     }
     ?>
 <a href="upload_solution.php" title=""></a>
@@ -154,6 +108,7 @@ function clicke(){
     alert("Please generate solution by terminal,then click upload");
 }
 
+
 </script>
 <script>
 
@@ -173,6 +128,6 @@ function foo () {
 </div>
 </div>
 </div>
-
+</div>
 </body>
 </html>
