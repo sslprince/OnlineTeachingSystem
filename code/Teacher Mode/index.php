5331e0198f243d94345d818da0ef37412d1f9044
Changchang Liu <changchang.liu01@student.adelaide.edu.au>
2017-04-02 19:37:16 +0930

    change index.php

--- code/Teacher Mode/index.php
+++ code/Teacher Mode/index.php
@@ -1,4 +1,4 @@
-
+<?php include 'header.php';?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,61 +10,16 @@
 <title>Memory Trace Teacher Mode</title>
 
 <!-- Bootstrap core CSS -->
-<link href="css/bootstrap.css" rel="stylesheet">
+
 
 <!-- Custom styles for this template -->
 <link href="css/teacher.css" rel="stylesheet">
 </head>
 
 <body>
-<nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
-<button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-<span class="navbar-toggler-icon"></span>
-</button>
-<a class="navbar-brand" href="#">Teacher Mode</a>
 
-<div class="collapse navbar-collapse" id="navbarsExampleDefault">
-<ul class="navbar-nav mr-auto">
-<li class="nav-item active">
-<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Student Mode</a>
-</li>
 
-<li class="nav-item">
-<a class="nav-link" href="#">Help</a>
-</li>
-</ul>
-<form class="form-inline mt-2 mt-md-0">
-<input class="form-control mr-sm-2" type="text" placeholder="Search">
-<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
-</div>
-</nav>
-
-<div class="container-fluid">
-<div class="row">
-<nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
-<ul class="nav nav-pills flex-column">
-<li class="nav-item">
-<a class="nav-link active" href="#">Overview <span class="sr-only">(current)</span></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="a.php">Upload C Code</a>
-
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Students' Marks</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#">Error Analytics</a>
-</li>
-</ul>
-
-</nav>
-
-<main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 <section class="row text-center places">
 <h1>Upload C Code</h1>
 <div class="col-6 col-sm-3 place">
