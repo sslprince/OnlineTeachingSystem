fa2f67f83ccb3e20e0b85a84c94f25f91d4e6f74
Changchang Liu <changchang.liu01@student.adelaide.edu.au>
2017-04-04 15:54:03 +0930

    change marks1 and add bar graph

--- code/Teacher Mode/marks1.php
+++ code/Teacher Mode/marks1.php
@@ -15,6 +15,10 @@
 
 <!-- Bootstrap core CSS -->
 <?php
+$xdata=array();
+$ydata=array();
+?>
+<?php
         $query = "SELECT distinct filename FROM Score";
         $res = mysqli_query($con,$query);
         $res1=null;
@@ -25,6 +29,7 @@
 
 <link rel="stylesheet" href="css/mark.css" type="text/css"/>
 <link rel="stylesheet" href="css/style.css" type="text/css"/>
+
 <script src="jquery-3.2.0.slim.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
@@ -34,6 +39,9 @@
 
 
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+
+  <div class ="parent">
   <form name="form1" enctype="multipart/form-data" method="post" action="">
   <label>
     <h1>Select</h1>
@@ -69,16 +77,29 @@ if( $_POST )
            <td>studentname</td>
            <td>score</td>
       </tr>
+      <?php $count = 0;?>
       <?php while($row = mysqli_fetch_assoc($res1)){
+        global $count;
+        global $xdata;
+        global $ydata;
+        $xdata[]=$row['studentname'];
+        $ydata[]=$row['score'];
         echo"<tr>";
 
-           echo"<td>".$row['studentname']."</td>";
-           echo"<td>".$row['score']."</td>";
+           echo"<td>".$xdata[$count]."</td>";
+           echo"<td>".$ydata[$count]."</td>";
 
         echo"</tr>";
+        $count++;
       }?>
 
 </table>
+
+<div class ="child">
+<img src="lalalal.jpg">
+</div>
+</div>
+
 <?php } ?>
 
 <header>
@@ -115,6 +136,37 @@ if( $_POST )
 
 </table>
 
+
+<?php
+require_once ("jpgraph-4.0.2/jpgraph/jpgraph.php");
+require_once ("jpgraph-4.0.2/jpgraph/jpgraph_bar.php");
+
+global $xdata;
+global $ydata;
+
+$graph = new Graph(300,200);  //创建新的Graph对象
+$graph->SetScale("textlin");  //刻度样式
+$graph->SetShadow();          //设置阴影
+$graph->img->SetMargin(40,20,30,40); //设置边距
+
+$graph->graph_theme = null; //设置主题为null，否则value->Show(); 无效
+
+$barplot = new BarPlot($ydata);  //创建BarPlot对象
+$barplot->SetFillColor('blue'); //设置颜色
+$barplot->value->Show(); //设置显示数字
+$graph->Add($barplot);  //将柱形图添加到图像中
+
+$graph->title->Set("Score Statistic");
+$graph->xaxis->title->Set("studentname"); //设置标题和X-Y轴标题
+$graph->yaxis->title->Set("Score");
+$graph->title->SetColor("red");
+$graph->title->SetMargin(10);
+$graph->xaxis->title->SetMargin(5);
+$graph->xaxis->SetTickLabels($xdata);
+
+
+$graph->Stroke("lalalal.jpg");
+?>
 
 
 <a href="upload_solution.php" title=""></a>
