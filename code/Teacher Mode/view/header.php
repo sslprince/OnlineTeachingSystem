b81f3a3dffeae9146e9e251563a0b08abacfcd79
Changchang Liu <changchang.liu01@student.adelaide.edu.au>
2017-05-22 15:22:19 +0930

    upload file for view code

---
+++ code/Teacher Mode/view/header.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/dashboard.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="../../index.php">Teacher Mode</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../upload/index.php">Home</a></li>
+            <li><a href="../../student_index.php">Student Mode</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <li class="active"><a href="../upload/index.php">Overview <span class="sr-only">(current)</span></a></li>
+            <li><a href="../view/viewcode.php">View C Code</a></li>
+            <li><a href="../upload/teacher_index.php">Edit C Code</a></li>
+            <li><a href="../marks/marks.php">Students' Marks</a></li>
+            <li><a href="../errors/errors.php">Error Analytics</a></li>
+            <li><a href="../student_information/information.php">Students' Information</a></li>
+          </ul>
+        </div>
+
+         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+           <section class="row text-center places">
+           <h1>View C Code</h1>
+           </section>
+    </div>
+</div>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ code/Teacher Mode/view/viewcode.php
@@ -0,0 +1,199 @@
+<?php include 'database.php';?>
+<?php include 'config/Config.php';?>
+<?php include 'header.php';?>
+<?php session_start();?>
+<?php
+  $db = new database();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="../../css/choose0.css" type="text/css" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="../../css/bootstrap.css" rel="stylesheet">
+  <link href="../../css/dashboard.css" rel="stylesheet">
+
+
+<title>Memory Trace Teacher Mode</title>
+
+<!-- Bootstrap core CSS -->
+
+<?php
+        $query = "SELECT distinct name FROM file";
+        $res = $db->select($query);
+        $res1=null;
+    ?>
+
+
+<!-- Custom styles for this template -->
+
+<link rel="stylesheet" href="../../css/mark.css" type="text/css"/>
+<link rel="stylesheet" href="../../css/style.css" type="text/css"/>
+
+<script src="jquery-3.2.0.slim.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+</head>
+
+<body>
+
+
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+
+
+<form name="form1" enctype="multipart/form-data" method="post" action="">
+    <label>
+    <caption><h2>Select filename</h2></caption>
+        <select name="select">
+
+              <?php while($row = mysqli_fetch_assoc($res)): ?>
+
+                <li><option value="<?php echo $row['name']?>"><?php echo $row['name']?></option></li>
+
+
+              <?php endwhile;?>
+
+
+          </select>
+        </label>
+  <label>
+    <input type="submit" class="btn btn-info" name="Submit" value="Results">
+  </label>
+</form>
+<div class ="parent">
+<div style="text-align:left"><h2>Code:</h2></div>
+<textarea name="comment" rows="15" cols="35">
+<?php
+
+if( $_POST )
+{
+
+  $a = $_POST['select'];
+  $query1 = "SELECT * FROM file  WHERE name = '$a'";
+  $res1 = $db->select($query1);
+  $start=100;
+  $count1=0;
+   while($row = mysqli_fetch_assoc($res1)){
+    //echo"<tr>";
+        $str1= $row['source_file'];
+      // echo"<td>".$row['source_file']."</td>";
+       //echo"<td>".$row['avg(score)']."</td>";
+       //echo"<td>".$row['MAX(score)']."</td>";
+       //echo"<td>".$row['MIN(score)']."</td>";
+
+    //echo"</tr>";
+
+  }
+  $code=(explode("\n",$str1));
+  for($i=0;$i<sizeof($code);$i++)                    //how many columns
+  {
+    if(!strcmp($code[$i],"int main(int argc, const char * argv[]) {"))
+    {
+      $start=$i;
+    }
+    if($i>=$start)
+    {
+      echo "$count1";
+      echo "    ";
+      echo "$code[$i]\n";
+      $count1++;
+    }
+    else
+    {
+      echo "    ";
+      echo "$code[$i]\n";
+    }
+  }
+?>
+</textarea>
+</div>
+<div class ="child">
+<div style="text-align:left"><h2>Solution:</h2></div>
+<table class = "table table-striped">
+     <tr>
+<?php
+$query1 = "SELECT `variable` FROM `file` WHERE name='$a' ";
+$res = $db->select($query1);
+$query2 = "SELECT `solution` FROM `file` WHERE name='$a' ";
+$res1 = $db->select($query2);
+$query3 = "SELECT `statement` FROM `file` WHERE name='$a' ";
+$res2 = $db->select($query3);
+$var = array();
+$var1 = array();
+$var2 = array();
+while($row = mysqli_fetch_assoc($res))
+{
+  foreach($row as $value)
+  {
+    $str=$value;
+  }
+}
+$var=(explode("\n",$str));
+echo "<td>statement(s)</td>";
+$size =   sizeof($var);                  //current statement
+for($i=0;$i<sizeof($var)-1;$i++)                    //how many columns
+{
+  echo "<td>$var[$i]</td>";
+}
+echo"</tr>";
+$index=0;
+while($row1 = mysqli_fetch_assoc($res1))
+{
+  foreach($row1 as $value1)
+  {
+    $str1=$value1;
+  }
+}
+$var1=(explode("\n",$str1));
+
+while($row2 = mysqli_fetch_assoc($res2))
+{
+  foreach($row2 as $value2)
+  {
+    $str2=$value2;
+  }
+}
+$var2=(explode("\n",$str2));
+$w=0;
+for($i=0;$i<sizeof($var1)-1;$i=$i+$size-1)
+                    //how many columns
+{
+
+  echo"<tr>";
+  echo "<td>$var2[$w]</td>";
+  for($j=0;$j<$size-1;$j++){
+    $z=$j+$i;
+    echo "<td>$var1[$z]</td>";
+  }
+  $w=$w+1;
+  echo"</tr>";
+}
+
+?>
+</tr>
+</table>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<input type="button" onClick="window.location.href='marks.php'" value="Back"/>
+
+
+
+
+
+
+
+</body>
+</html>
+<?php } ?>
